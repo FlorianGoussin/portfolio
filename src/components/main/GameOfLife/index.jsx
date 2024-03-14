c1a3fee86c6d07665f506a86c0b910bfb63ff4aa
Florian Goussin <florian.goussin@gmail.com>
2024-03-14 13:22:04 -0600

    fix bug by removing refresh state

--- src/components/main/GameOfLife/index.jsx
+++ src/components/main/GameOfLife/index.jsx
@@ -16,12 +16,55 @@ function isElementOnScreen(el) {
     return !(rect.bottom < 0 || rect.top - viewHeight >= 0)
 }
 
+class Grid {
+    constructor(arr, width) {
+        this.arr = arr
+        this.width = width
+    }
+    #getIndex(x, y) {
+        return y * this.width + x
+    }
+    get length() {
+        return this.arr.length
+    }
+    getCell(x, y) {
+        return this.arr[this.#getIndex(x, y)]
+    }
+    setCell(x, y, cell) {
+        this.arr[this.#getIndex(x, y)] = cell
+    }
+}
+
+class Cell {
+    constructor(ctx, x, y, randomize = false) {
+        this.ctx = ctx
+        this.x = x
+        this.y = y
+        this.alive = randomize ? Math.random() >= 0.7 : false
+        this.wasAlive = null
+    }
+    draw(ctx) {
+        // don't draw if the cell is unchanged
+        if (this.alive === this.wasAlive) {
+            return
+        }
+        ctx.beginPath()
+        ctx.rect(this.x, this.y, cellSize, cellSize)
+        if (this.alive) {
+            ctx.fillStyle = 'rgba(255,255,255,1)'
+        } else {
+            ctx.fillStyle = 'rgba(0,0,0,1)'
+        }
+        ctx.fill()
+    }
+}
+
 export const GameOfLife = () => {
     const canvasRef = useRef()
 
     const [width, setWidth] = useState(0)
     const [ctx, setCtx] = useState(null)
-    const [grid, setGrid] = useState([])
+    const [grid, setGrid] = useState(null)
 
     useEffect(() => {
         resetCanvas()
@@ -56,7 +99,6 @@ export const GameOfLife = () => {
                 }
             } else {
                 if (shouldSeedWhenHidden) {
-                    console.log('shouldSeedWhenHidden > reset canvas!')
                     const grid = createNewGrid({ randomize: true })
                     setGrid(grid)
                     shouldSeedWhenHidden = false
@@ -79,51 +121,64 @@ export const GameOfLife = () => {
         ctx2d.clearRect(0, 0, windowWidth, height)
     }
 
+    function createNewGrid({ randomize = false, modelGrid = null } = {}) {
+        const grid = new Grid([], width)
+        for (let x = 0; x < width; x += cellSize) {
+            for (let y = 0; y < height; y += cellSize) {
+                const cell = modelGrid
+                    ? modelGrid.getCell(x, y)
+                    : new Cell(ctx, x, y, randomize)
+                grid.setCell(x, y, cell)
+            }
+        }
+        return grid
+    }
+
     // http://www.conwaylife.com/wiki/Conway%27s_Game_of_Life#Rules
     function applyRules() {
-        const newGenGrid = createNewGrid() // new grid for this new generation
+        // Create a snapshot of the grid to base the new generation on
+        for (let x = 0; x < width; x += cellSize) {
+            for (let y = 0; y < height; y += cellSize) {
+                const cell = grid.getCell(x, y)
+                cell.wasAlive = cell.alive
+            }
+        }
 
         for (let x = 0; x < width; x += cellSize) {
             for (let y = 0; y < height; y += cellSize) {
                 // Get the current cell from the previous generation grid and
                 // get its alive neighbor count
                 const cell = grid.getCell(x, y)
-                const newCell = newGenGrid.getCell(x, y)
                 const aliveNeighbors = countAliveNeighbors(cell)
 
                 // cell alive
-                if (cell.alive) {
+                if (cell.wasAlive) {
                     // with fewer than two or move than three alive neighbors dies
                     if (aliveNeighbors < 2 || aliveNeighbors > 3) {
-                        newCell.alive = false
+                        cell.alive = false
                     }
                     // with two or three alive neighbors stays alive
                     if (aliveNeighbors === 2 || aliveNeighbors === 3) {
-                        newCell.alive = true
+                        cell.alive = true
                     }
                 } else {
                     // dead cell
                     // with exactly three alive neighbors goes back to life
                     if (aliveNeighbors === 3) {
-                        newCell.alive = true
+                        cell.alive = true
                     }
                 }
             }
         }
-        setGrid(newGenGrid)
+        drawGrid()
     }
 
-    function createNewGrid({ randomize = false, modelGrid = null } = {}) {
-        const grid = new Grid([])
+    function drawGrid() {
         for (let x = 0; x < width; x += cellSize) {
             for (let y = 0; y < height; y += cellSize) {
-                const cell = modelGrid
-                    ? modelGrid.getCell(x, y)
-                    : new Cell(x, y, randomize)
-                grid.setCell(x, y, cell)
+                grid.getCell(x, y).draw(ctx)
             }
         }
-        return grid
     }
 
     function countAliveNeighbors(cell) {
@@ -142,81 +197,40 @@ export const GameOfLife = () => {
         let count = 0
 
         if (leftConstraint) {
-            if (topConstraint && grid.getCell(prevCellX, prevCellY).alive) {
+            if (topConstraint && grid.getCell(prevCellX, prevCellY).wasAlive) {
                 count++
             }
-            if (grid.getCell(prevCellX, cell.y).alive) {
+            if (grid.getCell(prevCellX, cell.y).wasAlive) {
                 count++
             }
-            if (bottomConstraint && grid.getCell(prevCellX, nextCellY).alive) {
+            if (
+                bottomConstraint &&
+                grid.getCell(prevCellX, nextCellY).wasAlive
+            ) {
                 count++
             }
         }
         if (rightConstraint) {
-            if (topConstraint && grid.getCell(nextCellX, prevCellY).alive) {
+            if (topConstraint && grid.getCell(nextCellX, prevCellY).wasAlive) {
                 count++
             }
-            if (grid.getCell(nextCellX, cell.y).alive) {
+            if (grid.getCell(nextCellX, cell.y).wasAlive) {
                 count++
             }
-            if (bottomConstraint && grid.getCell(nextCellX, nextCellY).alive) {
+            if (
+                bottomConstraint &&
+                grid.getCell(nextCellX, nextCellY).wasAlive
+            ) {
                 count++
             }
         }
-        if (topConstraint && grid.getCell(cell.x, prevCellY).alive) {
+        if (topConstraint && grid.getCell(cell.x, prevCellY).wasAlive) {
             count++
         }
-        if (bottomConstraint && grid.getCell(cell.x, nextCellY).alive) {
+        if (bottomConstraint && grid.getCell(cell.x, nextCellY).wasAlive) {
             count++
         }
         return count
-    }
-
-    function drawGrid() {
-        // ctx.clearRect(0, 0, width, height)
-
-        for (let x = 0; x < width; x += cellSize) {
-            for (let y = 0; y < height; y += cellSize) {
-                grid.getCell(x, y).draw(ctx)
-            }
-        }
-    }
-
-    class Grid {
-        constructor(arr) {
-            this.arr = arr
-        }
-        #getIndex(x, y) {
-            return y * width + x
-        }
-        get length() {
-            return this.arr.length
-        }
-        getCell(x, y) {
-            return this.arr[this.#getIndex(x, y)]
-        }
-        setCell(x, y, cell) {
-            const index = y * width + x
-            this.arr[this.#getIndex(x, y)] = cell
-        }
-    }
-
-    class Cell {
-        constructor(x, y, randomize) {
-            this.x = x
-            this.y = y
-            this.alive = randomize ? Math.random() >= 0.7 : false
-        }
-        draw(ctx) {
-            ctx.beginPath()
-            ctx.rect(this.x, this.y, cellSize, cellSize)
-            if (this.alive) {
-                ctx.fillStyle = 'rgba(255,255,255,1)'
-            } else {
-                ctx.fillStyle = 'rgba(0,0,0,1)'
-            }
-            ctx.fill()
-        }
     }
 
     return <Canvas ref={canvasRef}></Canvas>
